8b71d598e9edcdd28de764220ef647f62ae8c7ad
Shubham Sahu <ss.cse1001@gmail.com>
2023-10-25 15:38:31 +0530

    change menu item design

--- menumate-frontend/src/pages/Home/index.tsx
+++ menumate-frontend/src/pages/Home/index.tsx
@@ -1,29 +1,29 @@
-// src/components/Menu.tsx
-
 import React from "react";
 import { menuItems } from "../../data/menuData";
 
 const Home: React.FC = () => {
   return (
-    <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="p-4">
       {menuItems.map((item: any) => (
         <div
           key={item.id}
-          className="bg-white shadow-md rounded-lg overflow-hidden"
+          className="flex items-center border-b border-gray-300 py-2"
         >
-          <img
-            src={item.image}
-            alt={item.name}
-            className="w-full h-48 object-cover"
-          />
-          <div className="p-4">
-            <h2 className="text-xl font-semibold mb-2">{item.name}</h2>
-            <p className="text-gray-600 mb-2">
+          <div className="w-1/4">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-16 h-16 object-cover rounded"
+            />
+          </div>
+          <div className="w-3/4 pl-4">
+            <h2 className="text-lg font-semibold">{item.name}</h2>
+            <p className="text-sm text-gray-600 mb-2">
               {item.isVeg ? "Veg" : "Non-Veg"}
             </p>
-            <p className="text-gray-600 mb-4">{item.description}</p>
-            <p className="text-xl font-semibold">${item.price}</p>
-            <button className="bg-blue-500 text-white px-4 py-2 mt-2">
+            <p className="text-sm text-gray-600 mb-2">{item.description}</p>
+            <p className="text-lg font-semibold">${item.price}</p>
+            <button className="bg-blue-500 text-white px-2 py-1 mt-2">
               Add to Cart
             </button>
           </div>
