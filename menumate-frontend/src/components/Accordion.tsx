d488c9407c6ac635f3e8076feb0f7cbc290b5cc6
Shubham Sahu <ss.cse1001@gmail.com>
2023-10-25 22:39:43 +0530

    add accordion,icons

---
+++ menumate-frontend/src/components/Accordion.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import {
+  AiOutlinePlusCircle,
+  AiOutlineDown,
+  AiOutlineUp,
+} from "react-icons/ai";
+import { BiSolidRectangle } from "react-icons/bi";
+
+interface AccordionProps {
+  title: string;
+  items: { name: string; description: string; isVeg: boolean; price: number }[];
+}
+
+const Accordion: React.FC<AccordionProps> = ({ title, items }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="mb-4">
+      <button
+        className="w-full bg-gray-200 p-2 text-left font-semibold"
+        onClick={toggleAccordion}
+      >
+        <div className="flex justify-between items-center">
+          {title}
+          {isOpen ? <AiOutlineUp /> : <AiOutlineDown />}
+        </div>
+      </button>
+      {isOpen && (
+        <div className="border border-gray-300 p-2">
+          {items.map((item, index) => (
+            <div key={index} className="flex justify-between my-3">
+              <div>
+                <p className="text-sm flex align-center">
+                  <BiSolidRectangle
+                    color={item.isVeg ? "green" : "red"}
+                    className="mt-1 mr-1 border border-black p-[1px]"
+                  />
+                  {item.name} -
+                  <div className="font-medium">Rs. {item.price}</div>
+                </p>
+                <p className="text-xs text-gray-600">{item.description}</p>
+              </div>
+              <button>
+                <AiOutlinePlusCircle color="green" />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Accordion;
--- menumate-frontend/src/pages/Home/index.tsx
+++ menumate-frontend/src/pages/Home/index.tsx
@@ -1,34 +1,26 @@
+// In your menu component
+
 import React from "react";
 import { menuItems } from "../../data/menuData";
+import Accordion from "../../components/Accordion";
 
 const Home: React.FC = () => {
+  const groupedMenuItems: { [key: string]: typeof menuItems } = {};
+
+  menuItems.forEach((item: any) => {
+    if (!groupedMenuItems[item.category]) {
+      groupedMenuItems[item.category] = [];
+    }
+    groupedMenuItems[item.category].push(item);
+  });
+
   return (
-    <div className="p-4">
-      {menuItems.map((item: any) => (
-        <div
-          key={item.id}
-          className="flex items-center border-b border-gray-300 py-2"
-        >
-          <div className="w-1/4">
-            <img
-              src={item.image}
-              alt={item.name}
-              className="w-16 h-16 object-cover rounded"
-            />
-          </div>
-          <div className="w-3/4 pl-4">
-            <h2 className="text-lg font-semibold">{item.name}</h2>
-            <p className="text-sm text-gray-600 mb-2">
-              {item.isVeg ? "Veg" : "Non-Veg"}
-            </p>
-            <p className="text-sm text-gray-600 mb-2">{item.description}</p>
-            <p className="text-lg font-semibold">${item.price}</p>
-            <button className="bg-blue-500 text-white px-2 py-1 mt-2">
-              Add to Cart
-            </button>
-          </div>
-        </div>
-      ))}
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="bg-white w-full max-w-md p-4">
+        {Object.entries(groupedMenuItems).map(([category, items]) => (
+          <Accordion key={category} title={category} items={items} />
+        ))}
+      </div>
     </div>
   );
 };
