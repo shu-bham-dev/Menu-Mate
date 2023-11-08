f12ef6d27e817088ab638705da7c3326b10993e6
Shubham Sahu <ss.cse1001@gmail.com>
2023-11-08 16:42:05 +0530

    config user controller, add endpoints

--- menumate-backend/src/user/user.controller.ts
+++ menumate-backend/src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get()
+  findAll() {
+    return {};
+  }
+
+  @Post()
+  createUser(@Body() user) {
+    return {};
+  }
+
+  @Get(':id')
+  findUsersByRestaurantId(@Param('id') id: string) {
+    return {};
+  }
+}
